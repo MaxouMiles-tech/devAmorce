cfdddf5a391f0d9a890276485404ca09ef234f11
MaxouMiles-tech <66181773+MaxouMiles-tech@users.noreply.github.com>
2020-11-20 12:20:48 +0100

    structure Bootstrap

---
+++ portfolio.php
@@ -0,0 +1,88 @@
+<!--------------------------------------------------------------------------------------------------------------------->
+<!--Portfolio-->
+<!--------------------------------------------------------------------------------------------------------------------->
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Zohra Boussouf</title>
+    <!-- <link rel="icon" href="img/favicon.png" /> -->
+    <!-- Bootstrap CSS -->
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" rel="stylesheet">
+    <link rel="stylesheet" href="app.css">
+</head>
+
+<body>
+<div class="site-header sticky-top">
+    <!-- banniere -->
+    <div class="d-flex justify-content-center" id="banniere">
+        <img src="logozb.png" alt="Zohra Boussouf" id="logozb">
+    </div>
+</div>
+
+
+<!-- corps du site  -->
+<div class="container-fluid ">
+    <div class="row justify-content-center">
+        <div class="col-md-3">
+            <div class="card shadow">
+                <img class="card-img-top" src="jarditou.png" alt="Photo représentant un e-commerce">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Jarditou</h5>
+                    <p class="card-text">
+                        Projet AFPA techniques de base 20103: création du site Web Jarditou suivant une maquette.
+                    </p>
+                    <a href="#" class="btn btn-success">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card shadow">
+                <img class="card-img-top" src="cvzb.png" alt="Photo représentant un CV">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Mon CV</h5>
+                    <p class="card-text">
+                        Projet AFPA techniques de base 20103: création d'un CV en ligne (première version).
+                    </p>
+                    <a href="#" class="btn btn-success">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card shadow">
+                <img class="card-img-top" src="photo.png" alt="Photo représentant de la photographie">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Jarditou</h5>
+                    <p class="card-text">
+                        Projet personnel : création d'un site Web pour un photographe professionnel suivant une
+                        maquette que j'ai realisée.
+                    </p>
+                    <a href="#" class="btn btn-success">Voir le site</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Optional JavaScript-->
+
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+
+
+</body>
+</html>
